b31e7a3192d6d0cf16cbf712414f37dc3c460fd6
MinHyuk SONG <88926519+S0NGMinHyuk@users.noreply.github.com>
2023-06-25 02:20:55 +0900

    2023.06.25
    그동안 궁금했던 body 내부에 여러 요소들을 집어넣는 것을 만들어봤다. 
    이제야 컨테이너, 사이즈드박스, 차일드와 칠드런에 대한 개념이 조금씩 잡혀가는 것 같지만 아직 완벽하지는 않다. 
    또한 컬럼, 로우, 센터 등등의 함수들도 아직은 어려워서 더 많은 예제들을 풀어봐야 할 것 같다.

---
+++ 버튼 연습/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false, // 앱 오른쪽 위 디버그 표시 제거
+      title: "BBANTO",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.green,
+        title: const Text("MainAxisAlignment.start",
+        style: TextStyle(
+          color: Colors.white,
+        ),
+        ),
+      ),
+      body: Column(
+        children: <Widget> [
+          Container(
+            margin: const EdgeInsets.all(5.0),
+            color: Colors.yellow[400],
+            child: const Row(
+              // mainAxisAlignment: MainAxisAlignment.spaceAround,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget> [
+                Icon(Icons.star, size: 50,),
+                Icon(Icons.star, size: 50,),
+                Icon(Icons.star, size: 50,),
+              ],
+            ),
+          ),
+          Center(
+            child: Column(
+              children: <Widget> [
+                SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                    ),
+                    onPressed: () {},
+                    child: const Text("Short"),
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                    ),
+                    onPressed: () {},
+                    child: const Text("A bit longer"),
+                  ),
+                ),
+                SizedBox(
+                  width: 200,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                    ),
+                    onPressed: () {},
+                    child: const Text("The longest text button"),
+                  ),
+                ),
+              ]
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
